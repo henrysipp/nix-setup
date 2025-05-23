64bd7a05b09b5d8e23b034e91213dd8c5461be9d
Henry Sipp <hesipp@gmail.com>
2025-05-23 16:28:56 -0500

    6   2025-05-23 10:46:05   (current)

--- rebuild.sh
+++ rebuild.sh
@@ -6,8 +6,8 @@ pushd ~/Developer/nix-setup
 alejandra . &>/dev/null
 git diff -U0 *.nix
 echo "NixOS Rebuilding..."
-sudo nixos-rebuild switch &>nixos-switch.log || (
- cat nixos-switch.log | grep --color error && false)
+sudo darwin-rebuild --switch &>nixos-switch.log || (
+ cat darwin-switch.log | grep --color error && false)
 gen=$(sudo darwin-rebuild --list-generations | grep current)
 git commit -am "$gen"
 popd
