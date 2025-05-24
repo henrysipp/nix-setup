3e88bbdc39424b73aabdbbdd2cd088358af2dcdd
Henry Sipp <hesipp@gmail.com>
2025-05-23 22:05:15 -0500

    NixOS Gen 35

--- modules/nixos/hyprland.nix
+++ modules/nixos/hyprland.nix
@@ -5,8 +5,14 @@
 
     programs.hyprland.withUWSM  = true;
 
+
     # environment.sessionVariables = {
     #     WLR_NO_HARDWARE_CURSORS = "1";
     #     NIXOS_OZONE_WL = "1";
     # };
+
+    environment.systemPackages = [
+        pkgs.kitty
+        pkgs.wofi
+    ];
 }
