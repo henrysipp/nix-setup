f7e09f7508bd4a8dcbee07a33f40af7ad6cd1aed
Henry Sipp <hesipp@gmail.com>
2025-05-24 14:41:02 -0500

    NixOS Gen 68

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -51,6 +51,11 @@
 
             decoration = {
                 rounding = 0;
+                rounding_power = 2;
+                active_opacity = 1 ;
+                inactive_opacity = 1; 
+                fullscreen_opacity = 1;
+
                 shadow = {
                     enabled = true;
                     range = 4;
