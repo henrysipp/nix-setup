d642e383c416491fa8db119c6ecf943348a72a35
Henry Sipp <hesipp@gmail.com>
2025-06-01 15:59:40 -0500

    Unfree hack for home

--- modules/hosts/gawain/default.nix
+++ modules/hosts/gawain/default.nix
@@ -14,6 +14,9 @@
       ++ config.flake.modules.nixosUsers.root.imports
       ++ config.flake.modules.nixosUsers.henry.imports
       ++ [
+        {
+          home-manager.users.henry.nixpkgs.config.allowUnfree = true;
+        }
         #Import home-manager modules for `henry` only.
         {
           home-manager.users.henry.imports = with config.flake.modules.homeManager; [
